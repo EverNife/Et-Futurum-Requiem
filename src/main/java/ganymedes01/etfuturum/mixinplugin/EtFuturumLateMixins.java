95cb05726fd798b788c67c3dc7989db768cad205
Pétrus Pradella <petrus_ant@hotmail.com>
2025-03-13 21:24:30 -0300

    Attempt to fix ExtraUtilities compat for finalCraft network

---
+++ src/main/java/ganymedes01/etfuturum/mixinplugin/EtFuturumLateMixins.java
@@ -0,0 +1,30 @@
+package ganymedes01.etfuturum.mixinplugin;
+
+import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
+import com.gtnewhorizon.gtnhmixins.LateMixin;
+import ganymedes01.etfuturum.Tags;
+import ganymedes01.etfuturum.configuration.configs.ConfigMixins;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@LateMixin
+public class EtFuturumLateMixins implements ILateMixinLoader {
+	@Override
+	public String getMixinConfig() {
+		return "mixins." + Tags.MOD_ID + ".late.json";
+	}
+
+	@Override
+	public List<String> getMixins(Set<String> loadedMods) {
+		List<String> mixins = new ArrayList<>();
+
+		if (loadedMods.contains("ExtraUtilities")) {
+			mixins.add("extrautilities.EventHandlerServerMixin");
+			mixins.add("extrautilities.ItemDivisionSigilMixin");
+		}
+
+		return mixins;
+	}
+}
